9522760883358467f750d5f015bde5b23f6e1713
mmengjuan <mengjuan.sun@xtransfer.cn>
2025-01-08 19:52:38 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+// Look at public/index.html!
+
+class App extends React.Component {
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "#44bb44"
+        }}
+      >
+        <View>
+          <Text color="#ffffff">Hello World</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+export default App;
